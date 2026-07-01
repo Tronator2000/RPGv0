b1a51e3d222d32eef2d9ff6956516025076765e3
Juan José Morante López <juanjo.morante.lopez@gmail.com>
2026-07-01 16:16:22 +0200

    Añadida clase Inventario

---
+++ src/rPGv0/Inventario.java
@@ -0,0 +1,10 @@
+package rPGv0;
+
+public class Inventario {
+
+	void Prueba(){
+
+	System.out.println("Me llamo Pepe");
+		
+	}
+}
--- src/rPGv0/Main.java
+++ src/rPGv0/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Enemigo[] enemigos = {Zombie}
+		//Enemigo[] enemigos = {}
 		
 		System.out.println("Ha aparecido un contrincante");
 		
+		Inventario inv = new Inventario();
+		inv.Prueba();
 	}
 
 }
